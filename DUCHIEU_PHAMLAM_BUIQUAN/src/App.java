ad1584e7d97bbefb95bfbf8846206f04c32d45a9
LamPham <lamcasio2000@gmail.com>
2025-08-22 17:30:55 +0700

    add nữa rồi

--- DUCHIEU_PHAMLAM_BUIQUAN/src/App.java
+++ DUCHIEU_PHAMLAM_BUIQUAN/src/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args){
         System.out.println("hi tôi biết commit rồi nè");
         System.out.println("tôi lại commit lại nha");
+        System.out.println("sao thích gì ");
     }
 }
