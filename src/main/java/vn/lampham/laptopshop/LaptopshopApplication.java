cdc003b35490fab62b196ababab6cfce36a7e5cf
LamPham <lamcasio2000@gmail.com>
2025-08-30 02:50:02 +0700

    push

--- src/main/java/vn/lampham/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/lampham/laptopshop/LaptopshopApplication.java
@@ -1,4 +1,4 @@
-package vn.hoidanit.laptopshop;
+package vn.lampham.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +8,6 @@ public class LaptopshopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaptopshopApplication.class, args);
-	}
+	} 
 
 }
