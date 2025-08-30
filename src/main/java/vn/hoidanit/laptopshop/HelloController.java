d676ea2d6f2113fd284ea4ecf1e9c6cfb0428b43
LamPham <lamcasio2000@gmail.com>
2025-08-30 21:20:33 +0700

    commit lại

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -1,3 +1,4 @@
+package vn.hoidanit.laptopshop;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -1,4 +1,4 @@
-package vn.lampham.laptopshop;
+package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
