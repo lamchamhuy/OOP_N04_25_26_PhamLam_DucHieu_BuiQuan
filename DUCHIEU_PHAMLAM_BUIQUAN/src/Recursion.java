dd9cf4c01d32b684b1464b5d1489d0760e504313
LamPham <lamcasio2000@gmail.com>
2025-09-08 18:19:20 +0700

    cập nhập time .... testtime,

--- DUCHIEU_PHAMLAM_BUIQUAN/src/Recursion.java
+++ DUCHIEU_PHAMLAM_BUIQUAN/src/Recursion.java
@@ -1,3 +1,16 @@
+package DUCHIEU_PHAMLAM_BUIQUAN.src;
+
 public class Recursion {
 
+    public int factorial(int n) {
+        if (n <= 1)
+            return 1;
+        return n * factorial(n - 1);
+    }
+
+    public int fibonacci(int n) {
+        if (n <= 1)
+            return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
--- DUCHIEU_PHAMLAM_BUIQUAN/test/TestRecursion.java
+++ DUCHIEU_PHAMLAM_BUIQUAN/test/TestRecursion.java
@@ -1,5 +1,17 @@
 package DUCHIEU_PHAMLAM_BUIQUAN.test;
 
-public class TestRecursion {
+import DUCHIEU_PHAMLAM_BUIQUAN.src.Recursion;
 
+public class TestRecursion {
+    public static void main(String[] args) {
+        Recursion r = new Recursion();
+
+        // Test giai thừa
+        int n = 5;
+        System.out.println("Factorial of " + n + " = " + r.factorial(n));
+
+        // Test Fibonacci
+        int f = 7;
+        System.out.println("Fibonacci of " + f + " = " + r.fibonacci(f));
+    }
 }
