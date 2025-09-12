4a7b7c77c4b4d350a8d1510e278e840d15e085c8
BuiMinhQuan1702 <23010725@st.phenikaa-uni.edu.vn>
2025-09-12 06:47:39 +0000

    reup danh ba dien thoai

--- DUCHIEU_PHAMLAM_BUIQUAN/src/interface.java
+++ DUCHIEU_PHAMLAM_BUIQUAN/src/interface.java
@@ -1,26 +1,3 @@
-public class NameNumber {
-    private String lastName;    // tên (last name)
-    private String telNumber;   // số điện thoại (telephone number)
-
-    // Default constructor
-    public NameNumber() {}
-
-    // Constructor with parameters
-    public NameNumber(String name, String num) {
-        lastName = name;
-        telNumber = num;
-    }
-
-    // Getter method for lastName
-    public String getLastName() {
-        return lastName;
-    }
-
-    // Getter method for telNumber
-    public String getTelNumber() {
-        return telNumber;
-    }
-}
 
 
 public class NNCollection {
