85606b9084e583c8d45880e23c93032f62e8cda2
LamPham <lamcasio2000@gmail.com>
2025-09-16 21:32:13 +0700

    đẩy thôi

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -1,12 +1,23 @@
 package vn.hoidanit.laptopshop;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 
 public class HelloController {
-    @GetMapping("/")
+	@GetMapping("/")
 	public String index() {
-		return "Hello World from Spring Boot!";
+		return "Hello World lam dep zai ";
 	}
 
+	@GetMapping("/user")
+	public String userPage() {
+		return "Only user can access this page ";
+	}
+
+	@GetMapping("/adim")
+	public String adimPage() {
+		return "Only adim can access this page ";
+	}
 }
