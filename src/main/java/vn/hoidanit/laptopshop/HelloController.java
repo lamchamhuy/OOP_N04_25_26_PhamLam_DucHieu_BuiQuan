49cbdb9a58bd31c9f9c40e700406b4fc09015e01
LamPham <lamcasio2000@gmail.com>
2025-10-03 13:53:46 +0700

    cập nhập controller

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,16 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@GetMapping("/")
 	public String index() {
-		return "Hello World lam dep zai ";
+		return "Hello World lam ";
 	}
-
 	@GetMapping("/user")
 	public String userPage() {
 		return "Only user can access this page ";
 	}
 
-	@GetMapping("/admim")
-	public String adimPage() {
+	@GetMapping("/admin")
+	public String adminPage() {
 		return "Only adim can access this page ";
 	}
 }
