4c9486ecdd1810aba12eb64d391eb163c1479f46
LamPham <lamcasio2000@gmail.com>
2025-10-06 16:44:05 +0700

    đẩy giao diện cơ bản lên

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,51 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+
+
+
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("hoidanit", "test form sevser hight");
+        return "hello";
+    }
+     @RequestMapping("/adim/user")
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("hoidanit", "test form sevser hight");
+        return "/admin/user/create";
+    }
+}
+
+// @RestController
+// public class UserController {
+
+
+//     private UserService userService;
+
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+//     @GetMapping("")
+//     public String getHomePage() {
+//         return this.userService.handleHello();
+//     }
+// }
